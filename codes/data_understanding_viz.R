802c145d18a43268c57836c872534d1781c79786
szutsattila <szuts.attila97@gmail.com>
2021-03-01 22:52:51 +0100

    add plot_exporter

--- codes/data_understanding_viz.R
+++ codes/data_understanding_viz.R
@@ -5,6 +5,7 @@ library(GGally)
 library(ggsci)
 library(ggpubr)
 library(scales)
+source("codes/helper.R")
 
 df <- readRDS('data/clean/west-coast-protein_clean.rds')
 
@@ -96,7 +97,7 @@ df %>%
   theme(legend.position = c(0.1, 0.9), legend.title = NULL, legend.background = element_rect(fill = "transparent", color = "transparent"))
 
 # by key accounts
-df %>% 
+pl_sc_ka <- df %>% 
   filter(last_year_sales != 0) %>% 
   ggplot(aes(sales_this_year, last_year_sales, color = key_accounts)) + 
   geom_smooth(alpha = 0.2, show.legend = F) + 
@@ -113,10 +114,10 @@ df %>%
        subtitle = "Conditional on Key Accounts") + 
   theme_classic() + 
   theme(legend.position = c(0.1, 0.9), legend.title = NULL, legend.background = element_rect(fill = "transparent", color = "transparent"))
-
+pl_sc_ka
 
 # by share of wallet
-df %>% 
+pl_sc_sw <- df %>% 
   filter(last_year_sales != 0) %>% 
   ggplot(aes(sales_this_year, last_year_sales, color = ifelse(company_p_in_shops < 0.5, "<50%", ">=50%"))) + 
   geom_smooth(alpha = 0.2, show.legend = F) + 
@@ -133,9 +134,11 @@ df %>%
        subtitle = "Conditional on Share of Wallet") + 
   theme_classic() + 
   theme(legend.position = c(0.1, 0.9), legend.title = NULL, legend.background = element_rect(fill = "transparent", color = "transparent"))
+pl_sc_sw
+
 
 # share of wallet distribution
-df  %>% ggplot(aes(company_p_in_shops)) + 
+pl_hi_sw <- df  %>% ggplot(aes(company_p_in_shops)) + 
   geom_histogram(aes(y = stat(count) / sum(count)), bins = 30, fill = "#5A9599FF", color = "black", alpha = 0.7) + 
   scale_y_continuous(labels = scales::percent) + 
   scale_x_continuous(labels = scales::percent) + 
@@ -143,9 +146,10 @@ df  %>% ggplot(aes(company_p_in_shops)) +
   labs(x = "Share of wallet",
        y = "Frequency",
        title = "Partner share of wallet as estimated by employees")
+pl_hi_sw
 
 # by stand alone shop (or multiple)
-df %>% 
+pl_sc_sh <- df %>% 
   filter(last_year_sales != 0) %>% 
   ggplot(aes(sales_this_year, last_year_sales, color = stand_alone_shops)) + 
   geom_smooth(alpha = 0.2, show.legend = F) + 
@@ -162,10 +166,10 @@ df %>%
        subtitle = "Conditional on number of shops") + 
   theme_classic() + 
   theme(legend.position = c(0.2, 0.9), legend.title = NULL, legend.background = element_rect(fill = "transparent", color = "transparent"))
-
+pl_sc_sh
 
 # by relationship quality
-df %>% 
+pl_sc_rq <- df %>% 
   filter(last_year_sales != 0) %>% 
   ggplot(aes(sales_this_year, last_year_sales, color = ifelse(relationship_quality %in% c(1, 2, 3), "Bad", "Good"))) + 
   geom_smooth(alpha = 0.2, show.legend = F) + 
@@ -182,9 +186,9 @@ df %>%
        subtitle = "Conditional on relatiionship quality") + 
   theme_classic() + 
   theme(legend.position = c(0.1, 0.9), legend.title = NULL, legend.background = element_rect(fill = "transparent", color = "transparent"))
+pl_sc_rq
 
-
-df %>% 
+pl_hi_rq <- df %>% 
   ggplot(aes(relationship_quality)) + 
   geom_bar(aes(y = stat(count) / sum(count)), stat="count", fill = "#5A9599FF", alpha = 0.7, color = "black") + 
   theme_classic() +
@@ -192,6 +196,7 @@ df %>%
        y = "Frequency", 
        title = "Distribution of Relationship quality among partners") + 
   scale_y_continuous(labels = scales::percent) 
+pl_hi_rq
 
 # by partner profile
 w <- df %>% 
@@ -254,7 +259,7 @@ d <- df %>%
   labs(x = NULL, y = NULL) + 
   theme_classic()
 
-annotate_figure(
+pl_sc_st <- annotate_figure(
   ggarrange(w, s, g, d, 
             labels = c("Webshop", "Shop", "Gym", "Distributor"),
             label.x = 0.05,
@@ -263,6 +268,7 @@ annotate_figure(
   bottom = "Sales this year, in USD", 
   left = "Sales last year, in USD", 
   top = "Correlation between sales this year and last year")
+pl_sc_st
 
 # df %>% ggplot(aes(sales_this_year, last_year_sales)) + 
 #   geom_smooth() + 
@@ -308,7 +314,7 @@ df %>% ggplot(aes(log(sales_this_year), log(gm_this_year))) +
 ggcorr(df)
 
 
-df %>% 
+pl_bx_ka <- df %>% 
   ggplot(aes(key_accounts, protein_gm_this_year)) + 
   geom_boxplot() + 
   theme_classic() + 
@@ -319,7 +325,10 @@ df %>%
                 labels = trans_format("log10", math_format(10^.x)),
                 limits = c(1, 10^7)) + 
   scale_x_discrete(labels = c("Not Key Account", "Key Account"))
-  
+pl_bx_ka  
 
 # those accounts that fall in the 95% range of key account protein sales should be considered as potential KAs. --> good for validation
 sd(df[df$key_accounts == 1, ]$protein_gm_this_year)
+
+# export all plots in bulk
+plot_export()
